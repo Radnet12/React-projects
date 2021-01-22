619541383e9d0c735e64fa52cd50e96c82a96d41
Sergey <sergeyvasylenko4@gmail.com>
2021-01-22 17:57:39 +0200

    add desing of cards

--- 06_Movie Site/movie/src/App.jsx
+++ 06_Movie Site/movie/src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Cards } from "./components/UI/Cards/Cards";
 import { Container } from "./components/UI/Container/Container";
 import { Header } from "./components/UI/Header/Header";
 import { Main } from "./components/UI/Main/Main";
@@ -8,11 +9,11 @@ import { Sidebar } from "./components/UI/Sidebar/Sidebar";
 function App(props) {
     const movies = async () => {
         const response = await fetch(
-            "https://api.themoviedb.org/3/movie/550?api_key=5daf90e431960f20b1aca24657c54316&language=ru-RU"
+            "https://api.themoviedb.org/3/movie/550?api_key=5daf90e431960f20b1aca24657c54316&language=en-US"
         );
         const data = await response.json();
 
-        console.log(data, props);
+        console.log(data);
     };
 
     movies();
@@ -24,6 +25,7 @@ function App(props) {
                 <Container>
                     <MainTemplate>
                         <Sidebar />
+                        <Cards/>
                     </MainTemplate>
                 </Container>
             </Main>
--- 06_Movie Site/movie/src/components/UI/Cards/Cards.jsx
+++ 06_Movie Site/movie/src/components/UI/Cards/Cards.jsx
@@ -1,21 +1,106 @@
-import React from 'react';
-import s from './Cards.module.scss';
+import React from "react";
+import s from "./Cards.module.scss";
 
 export const Cards = () => {
     return (
         <div className={s.cards}>
             <ul className={s.cards__list}>
                 <li className={s.item}>
-                    <a className={s.item__link}>
-                        <div className={s.item__image}>
-                            <img src="" alt=""/>
-                        </div>
+                    <div className={s.item__image}>
+                        <a href="#" className={s.item__link}>
+                            <img
+                                src="https://image.tmdb.org/t/p/w342/bptfVGEQuv6vDTIMVCHjJ9Dz8PX.jpg"
+                                alt=""
+                            />
+                        </a>
+                    </div>
+                    <div className={s.item__rating}>
+                        <span>7.8</span>
+                    </div>
+                    <div className={s.item__bottom}>
                         <div className={s.item__title}>
-                            Бойцовский клуб
+                            <a href="#">Бойцовский клуб</a>
                         </div>
-                    </a>
+                        <div className={s.item__date}>18.03.20</div>
+                    </div>
+                </li>
+                <li className={s.item}>
+                    <div className={s.item__image}>
+                        <a href="#" className={s.item__link}>
+                            <img
+                                src="https://image.tmdb.org/t/p/w342/bptfVGEQuv6vDTIMVCHjJ9Dz8PX.jpg"
+                                alt=""
+                            />
+                        </a>
+                    </div>
+                    <div className={s.item__rating}>
+                        <span>7.8</span>
+                    </div>
+                    <div className={s.item__bottom}>
+                        <div className={s.item__title}>
+                            <a href="#">Бойцовский клуб</a>
+                        </div>
+                        <div className={s.item__date}>18.03.20</div>
+                    </div>
+                </li>
+                <li className={s.item}>
+                    <div className={s.item__image}>
+                        <a href="#" className={s.item__link}>
+                            <img
+                                src="https://image.tmdb.org/t/p/w342/bptfVGEQuv6vDTIMVCHjJ9Dz8PX.jpg"
+                                alt=""
+                            />
+                        </a>
+                    </div>
+                    <div className={s.item__rating}>
+                        <span>7.8</span>
+                    </div>
+                    <div className={s.item__bottom}>
+                        <div className={s.item__title}>
+                            <a href="#">Бойцовский клуб</a>
+                        </div>
+                        <div className={s.item__date}>18.03.20</div>
+                    </div>
+                </li>
+                <li className={s.item}>
+                    <div className={s.item__image}>
+                        <a href="#" className={s.item__link}>
+                            <img
+                                src="https://image.tmdb.org/t/p/w342/bptfVGEQuv6vDTIMVCHjJ9Dz8PX.jpg"
+                                alt=""
+                            />
+                        </a>
+                    </div>
+                    <div className={s.item__rating}>
+                        <span>7.8</span>
+                    </div>
+                    <div className={s.item__bottom}>
+                        <div className={s.item__title}>
+                            <a href="#">Бойцовский клуб</a>
+                        </div>
+                        <div className={s.item__date}>18.03.20</div>
+                    </div>
+                </li>
+                <li className={s.item}>
+                    <div className={s.item__image}>
+                        <a href="#" className={s.item__link}>
+                            <img
+                                src="https://image.tmdb.org/t/p/w342/bptfVGEQuv6vDTIMVCHjJ9Dz8PX.jpg"
+                                alt=""
+                            />
+                        </a>
+                    </div>
+                    <div className={s.item__rating}>
+                        <span>7.8</span>
+                    </div>
+                    <div className={s.item__bottom}>
+                        <div className={s.item__title}>
+                            <a href="#">Бойцовский клуб</a>
+                        </div>
+                        <div className={s.item__date}>18.03.20</div>
+                    </div>
                 </li>
             </ul>
         </div>
-    )
+    );
 };
