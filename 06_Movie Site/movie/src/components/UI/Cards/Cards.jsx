826db4fd4fee6006d846364d4fee4830263800f4
Sergey <sergeyvasylenko4@gmail.com>
2021-01-23 20:11:26 +0200

    finished card for movie

--- 06_Movie Site/movie/src/components/UI/Cards/Cards.jsx
+++ 06_Movie Site/movie/src/components/UI/Cards/Cards.jsx
@@ -23,7 +23,25 @@ export const Cards = () => {
                         </div>
                         <div className={s.item__date}>18.03.20</div>
                     </div>
-                    <div className={s.item__hovered}></div>
+                    <div className={s.item__hovered}>
+                        <div className={s.item__info}>
+                            <p>
+                                Название: <span>Бойцовский клуб</span>
+                            </p>
+                            <p>
+                                Слоган: <span>Месть...</span>
+                            </p>
+                            <p>
+                                Сезонов: <span>6</span>
+                            </p>
+                            <p>
+                                Жанры: <span>Триллер, Ужасы, Фантастика</span>
+                            </p>
+                            <p>
+                                Дата выхода: <span>18.02.1999</span>
+                            </p>
+                        </div>
+                    </div>
                 </li>
                 <li className={s.item}>
                     <div className={s.item__image}>
@@ -43,7 +61,25 @@ export const Cards = () => {
                         </div>
                         <div className={s.item__date}>18.03.20</div>
                     </div>
-                    <div className={s.item__hovered}></div>
+                    <div className={s.item__hovered}>
+                        <div className={s.item__info}>
+                            <p>
+                                Название: <span>Бойцовский клуб</span>
+                            </p>
+                            <p>
+                                Слоган: <span>Месть...</span>
+                            </p>
+                            <p>
+                                Сезонов: <span>6</span>
+                            </p>
+                            <p>
+                                Жанры: <span>Триллер, Ужасы, Фантастика</span>
+                            </p>
+                            <p>
+                                Дата выхода: <span>18.02.1999</span>
+                            </p>
+                        </div>
+                    </div>
                 </li>
                 <li className={s.item}>
                     <div className={s.item__image}>
@@ -63,7 +99,25 @@ export const Cards = () => {
                         </div>
                         <div className={s.item__date}>18.03.20</div>
                     </div>
-                    <div className={s.item__hovered}></div>
+                    <div className={s.item__hovered}>
+                        <div className={s.item__info}>
+                            <p>
+                                Название: <span>Бойцовский клуб</span>
+                            </p>
+                            <p>
+                                Слоган: <span>Месть...</span>
+                            </p>
+                            <p>
+                                Сезонов: <span>6</span>
+                            </p>
+                            <p>
+                                Жанры: <span>Триллер, Ужасы, Фантастика</span>
+                            </p>
+                            <p>
+                                Дата выхода: <span>18.02.1999</span>
+                            </p>
+                        </div>
+                    </div>
                 </li>
                 <li className={s.item}>
                     <div className={s.item__image}>
@@ -83,7 +137,253 @@ export const Cards = () => {
                         </div>
                         <div className={s.item__date}>18.03.20</div>
                     </div>
-                    <div className={s.item__hovered}></div>
+                    <div className={s.item__hovered}>
+                        <div className={s.item__info}>
+                            <p>
+                                Название: <span>Бойцовский клуб</span>
+                            </p>
+                            <p>
+                                Слоган: <span>Месть...</span>
+                            </p>
+                            <p>
+                                Сезонов: <span>6</span>
+                            </p>
+                            <p>
+                                Жанры: <span>Триллер, Ужасы, Фантастика</span>
+                            </p>
+                            <p>
+                                Дата выхода: <span>18.02.1999</span>
+                            </p>
+                        </div>
+                    </div>
+                </li>
+                <li className={s.item}>
+                    <div className={s.item__image}>
+                        <a href="#" className={s.item__link}>
+                            <img
+                                src="https://image.tmdb.org/t/p/w342/bptfVGEQuv6vDTIMVCHjJ9Dz8PX.jpg"
+                                alt=""
+                            />
+                        </a>
+                    </div>
+                    <div className={s.item__rating}>
+                        <span>7.8</span>
+                    </div>
+                    <div className={s.item__bottom}>
+                        <div className={s.item__title}>
+                            <a href="#">Бойцовский клуб</a>
+                        </div>
+                        <div className={s.item__date}>18.03.20</div>
+                    </div>
+                    <div className={s.item__hovered}>
+                        <div className={s.item__info}>
+                            <p>
+                                Название: <span>Бойцовский клуб</span>
+                            </p>
+                            <p>
+                                Слоган: <span>Месть...</span>
+                            </p>
+                            <p>
+                                Сезонов: <span>6</span>
+                            </p>
+                            <p>
+                                Жанры: <span>Триллер, Ужасы, Фантастика</span>
+                            </p>
+                            <p>
+                                Дата выхода: <span>18.02.1999</span>
+                            </p>
+                        </div>
+                    </div>
+                </li>
+                <li className={s.item}>
+                    <div className={s.item__image}>
+                        <a href="#" className={s.item__link}>
+                            <img
+                                src="https://image.tmdb.org/t/p/w342/bptfVGEQuv6vDTIMVCHjJ9Dz8PX.jpg"
+                                alt=""
+                            />
+                        </a>
+                    </div>
+                    <div className={s.item__rating}>
+                        <span>7.8</span>
+                    </div>
+                    <div className={s.item__bottom}>
+                        <div className={s.item__title}>
+                            <a href="#">Бойцовский клуб</a>
+                        </div>
+                        <div className={s.item__date}>18.03.20</div>
+                    </div>
+                    <div className={s.item__hovered}>
+                        <div className={s.item__info}>
+                            <p>
+                                Название: <span>Бойцовский клуб</span>
+                            </p>
+                            <p>
+                                Слоган: <span>Месть...</span>
+                            </p>
+                            <p>
+                                Сезонов: <span>6</span>
+                            </p>
+                            <p>
+                                Жанры: <span>Триллер, Ужасы, Фантастика</span>
+                            </p>
+                            <p>
+                                Дата выхода: <span>18.02.1999</span>
+                            </p>
+                        </div>
+                    </div>
+                </li>
+                <li className={s.item}>
+                    <div className={s.item__image}>
+                        <a href="#" className={s.item__link}>
+                            <img
+                                src="https://image.tmdb.org/t/p/w342/bptfVGEQuv6vDTIMVCHjJ9Dz8PX.jpg"
+                                alt=""
+                            />
+                        </a>
+                    </div>
+                    <div className={s.item__rating}>
+                        <span>7.8</span>
+                    </div>
+                    <div className={s.item__bottom}>
+                        <div className={s.item__title}>
+                            <a href="#">Бойцовский клуб</a>
+                        </div>
+                        <div className={s.item__date}>18.03.20</div>
+                    </div>
+                    <div className={s.item__hovered}>
+                        <div className={s.item__info}>
+                            <p>
+                                Название: <span>Бойцовский клуб</span>
+                            </p>
+                            <p>
+                                Слоган: <span>Месть...</span>
+                            </p>
+                            <p>
+                                Сезонов: <span>6</span>
+                            </p>
+                            <p>
+                                Жанры: <span>Триллер, Ужасы, Фантастика</span>
+                            </p>
+                            <p>
+                                Дата выхода: <span>18.02.1999</span>
+                            </p>
+                        </div>
+                    </div>
+                </li>
+                <li className={s.item}>
+                    <div className={s.item__image}>
+                        <a href="#" className={s.item__link}>
+                            <img
+                                src="https://image.tmdb.org/t/p/w342/bptfVGEQuv6vDTIMVCHjJ9Dz8PX.jpg"
+                                alt=""
+                            />
+                        </a>
+                    </div>
+                    <div className={s.item__rating}>
+                        <span>7.8</span>
+                    </div>
+                    <div className={s.item__bottom}>
+                        <div className={s.item__title}>
+                            <a href="#">Бойцовский клуб</a>
+                        </div>
+                        <div className={s.item__date}>18.03.20</div>
+                    </div>
+                    <div className={s.item__hovered}>
+                        <div className={s.item__info}>
+                            <p>
+                                Название: <span>Бойцовский клуб</span>
+                            </p>
+                            <p>
+                                Слоган: <span>Месть...</span>
+                            </p>
+                            <p>
+                                Сезонов: <span>6</span>
+                            </p>
+                            <p>
+                                Жанры: <span>Триллер, Ужасы, Фантастика</span>
+                            </p>
+                            <p>
+                                Дата выхода: <span>18.02.1999</span>
+                            </p>
+                        </div>
+                    </div>
+                </li>
+                <li className={s.item}>
+                    <div className={s.item__image}>
+                        <a href="#" className={s.item__link}>
+                            <img
+                                src="https://image.tmdb.org/t/p/w342/bptfVGEQuv6vDTIMVCHjJ9Dz8PX.jpg"
+                                alt=""
+                            />
+                        </a>
+                    </div>
+                    <div className={s.item__rating}>
+                        <span>7.8</span>
+                    </div>
+                    <div className={s.item__bottom}>
+                        <div className={s.item__title}>
+                            <a href="#">Бойцовский клуб</a>
+                        </div>
+                        <div className={s.item__date}>18.03.20</div>
+                    </div>
+                    <div className={s.item__hovered}>
+                        <div className={s.item__info}>
+                            <p>
+                                Название: <span>Бойцовский клуб</span>
+                            </p>
+                            <p>
+                                Слоган: <span>Месть...</span>
+                            </p>
+                            <p>
+                                Сезонов: <span>6</span>
+                            </p>
+                            <p>
+                                Жанры: <span>Триллер, Ужасы, Фантастика</span>
+                            </p>
+                            <p>
+                                Дата выхода: <span>18.02.1999</span>
+                            </p>
+                        </div>
+                    </div>
+                </li>
+                <li className={s.item}>
+                    <div className={s.item__image}>
+                        <a href="#" className={s.item__link}>
+                            <img
+                                src="https://image.tmdb.org/t/p/w342/bptfVGEQuv6vDTIMVCHjJ9Dz8PX.jpg"
+                                alt=""
+                            />
+                        </a>
+                    </div>
+                    <div className={s.item__rating}>
+                        <span>7.8</span>
+                    </div>
+                    <div className={s.item__bottom}>
+                        <div className={s.item__title}>
+                            <a href="#">Бойцовский клуб</a>
+                        </div>
+                        <div className={s.item__date}>18.03.20</div>
+                    </div>
+                    <div className={s.item__hovered}>
+                        <div className={s.item__info}>
+                            <p>
+                                Название: <span>Бойцовский клуб</span>
+                            </p>
+                            <p>
+                                Слоган: <span>Месть...</span>
+                            </p>
+                            <p>
+                                Сезонов: <span>6</span>
+                            </p>
+                            <p>
+                                Жанры: <span>Триллер, Ужасы, Фантастика</span>
+                            </p>
+                            <p>
+                                Дата выхода: <span>18.02.1999</span>
+                            </p>
+                        </div>
+                    </div>
                 </li>
             </ul>
         </div>
