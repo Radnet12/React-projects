ecaa63878dfc81aee2b8a375a7c436e5607f9b31
Sergey <sergeyvasylenko4@gmail.com>
2021-01-25 19:04:10 +0200

    add close/open to sidebar

--- 06_Movie Site/movie/src/components/UI/Sidebar/Sidebar.jsx
+++ 06_Movie Site/movie/src/components/UI/Sidebar/Sidebar.jsx
@@ -1,18 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import s from "./Sidebar.module.scss";
 import { NavLink } from "react-router-dom";
 
 export const Sidebar = ({ genres, sort, filterMovies, genreFormat, sortMovies }) => {
+    const [isFilterClosed, setIsFilterClosed] = useState(false);
     const capitalizeFirstLetter = (name) => {
         return name.charAt(0).toUpperCase() + name.substring(1);
     };
 
+    const closeSidebar = () => {
+        setIsFilterClosed((prevState) => !prevState);
+    }
     return (
-        <div className={s.sidebar}>
-            <h2 className={s.sidebar__title}>Фильтры</h2>
+        <div
+            className={`${s.sidebar} ${isFilterClosed ? s.sidebar_close : ""}`}
+        >
+            <h2 className={s.sidebar__title} onClick={closeSidebar}>
+                Фильтры
+            </h2>
             <div className={s.sidebar__item}>
                 <div className={s.sidebar__main}>
-                    <h3 className={s.sidebar__subtitle}>Жанр</h3>
+                    <h3 className={s.sidebar__subtitle}>
+                        Жанр
+                    </h3>
                     <ul className={s.sidebar__list}>
                         {genres.map((genre) => {
                             return (
@@ -38,7 +48,9 @@ export const Sidebar = ({ genres, sort, filterMovies, genreFormat, sortMovies })
             </div>
             <div className={s.sidebar__item}>
                 <div className={s.sidebar__main}>
-                    <h3 className={s.sidebar__subtitle}>Отсортируйте</h3>
+                    <h3 className={s.sidebar__subtitle}>
+                        Отсортируйте
+                    </h3>
                     <ul className={s.sidebar__list}>
                         {sort.map((item, index) => {
                             return (
