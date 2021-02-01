fedddfb3812102829591be26e0eaec694ac3ee82
Sergey <sergeyvasylenko4@gmail.com>
2021-02-01 17:28:38 +0200

    add catalog details to movie page

--- 06_Movie Site/movie/src/components/CatalogPage/CatalogDetails/CatalogDetails.jsx
+++ 06_Movie Site/movie/src/components/CatalogPage/CatalogDetails/CatalogDetails.jsx
@@ -1,6 +1,80 @@
-import React from 'react';
-import s from './CatalogDetails.module.scss';
+import React from "react";
+import s from "./CatalogDetails.module.scss";
 
-export const CatalogDetails = () => {
-    return <section className={s.left}>dfdsf</section>;
+export const CatalogDetails = ({ cast, crew }) => {
+    if (cast !== undefined && crew !== undefined) {
+        return (
+            <section className={s.details}>
+                <ul className={s.details__tabs}>
+                    <li className={s.details__tabs_item}>Актёрский состав</li>
+                    <li className={s.details__tabs_item}>Съемочная группа</li>
+                    <li className={s.details__tabs_item}>Отзывы</li>
+                </ul>
+                <div>
+                    <div className={s.details__content}>
+                        <ul className={s.person}>
+                            {cast.map(person => {
+                                return (
+                                    <li key={person.cast_id} className={s.person__item}>
+                                        <div className={s.person__image}>
+                                            <img
+                                                src={
+                                                    person.profile_path !== null
+                                                        ? `https://image.tmdb.org/t/p/w45${person.profile_path}`
+                                                        : "https://dummyimage.com/45x68/a6a6a6/fff.jpg&text=%D0%9D%D0%B5%D1%82+%D0%BF%D0%BE%D1%81%D1%82%D0%B5%D1%80%D0%B0"
+                                                }
+                                                alt={person.name}
+                                                title={person.name}
+                                                loading="lazy"
+                                            />
+                                        </div>
+                                        <div className={s.person__info}>
+                                            <div className={s.person__detail}>
+                                                <span>Имя:</span> {person.name}
+                                            </div>
+                                            <div className={s.person__detail}>
+                                                <span>Персонаж:</span> {person.character}
+                                            </div>
+                                        </div>
+                                    </li>
+                                );
+                            })}
+                        </ul>
+                    </div>
+                    <div className={s.details__content}>
+                        <ul className={s.person}>
+                            {crew.map((person) => {
+                                return (
+                                    <li key={person.id} className={s.person__item}>
+                                        <div className={s.person__image}>
+                                            <img
+                                                src={
+                                                    person.profile_path !== null
+                                                        ? `https://image.tmdb.org/t/p/w45${person.profile_path}`
+                                                        : "https://dummyimage.com/45x68/a6a6a6/fff.jpg&text=%D0%9D%D0%B5%D1%82+%D0%BF%D0%BE%D1%81%D1%82%D0%B5%D1%80%D0%B0"
+                                                }
+                                                alt={person.name}
+                                                title={person.name}
+                                                loading="lazy"
+                                            />
+                                        </div>
+                                        <div className={s.person__info}>
+                                            <div className={s.person__detail}>
+                                                <span>Имя:</span> {person.name}
+                                            </div>
+                                            <div className={s.person__detail}>
+                                                <span>Должность:</span> {person.job}
+                                            </div>
+                                        </div>
+                                    </li>
+                                );
+                            })}
+                        </ul>
+                    </div>
+                </div>
+            </section>
+        );
+    } else {
+        return null;
+    }
 };
--- 06_Movie Site/movie/src/components/CatalogPage/CatalogPage.jsx
+++ 06_Movie Site/movie/src/components/CatalogPage/CatalogPage.jsx
@@ -7,15 +7,26 @@ import { CatalogSidebar } from "./CatalogSidebar/CatalogSidebar";
 
 export const CatalogPage = () => {
     const [film, setFilm] = useState([]);
+    const [credits, setCredits] = useState([]);
     console.log(film);
     useEffect(() => {
-        fetch(
-            // "https://api.themoviedb.org/3/configuration?api_key=5daf90e431960f20b1aca24657c54316"
-            "https://api.themoviedb.org/3/movie/775996?api_key=5daf90e431960f20b1aca24657c54316&language=ru-RU"
-            // "https://api.themoviedb.org/3/movie/775996/credits?api_key=5daf90e431960f20b1aca24657c54316&language=ru-RU"
-        )
-            .then((response) => response.json())
-            .then((data) => setFilm(data));
+        const loadMovie = () => {
+            fetch(
+                // "https://api.themoviedb.org/3/configuration?api_key=5daf90e431960f20b1aca24657c54316"
+                "https://api.themoviedb.org/3/movie/775996?api_key=5daf90e431960f20b1aca24657c54316&language=ru-RU"
+            )
+                .then((response) => response.json())
+                .then((data) => setFilm(data));
+        };
+        const loadCredits = () => {
+            fetch(
+                "https://api.themoviedb.org/3/movie/775996/credits?api_key=5daf90e431960f20b1aca24657c54316&language=ru-RU"
+            )
+                .then((response) => response.json())
+                .then((data) => setCredits(data));
+        };
+        loadMovie();
+        loadCredits();
     }, []);
     return (
         <>
@@ -23,7 +34,7 @@ export const CatalogPage = () => {
             <div className={s.bottom}>
                 <Container>
                     <div className={s.bottom__wrapper}>
-                        <CatalogDetails/>
+                        <CatalogDetails cast={credits.cast} crew={credits.crew}/>
                         <CatalogSidebar/>
                     </div>
                 </Container>
